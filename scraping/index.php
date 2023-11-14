b21d97bc19e5fb8162963ded4e3cda66ea476333
PuzzledStone <ianxd03@gmail.com>
2023-11-14 13:58:12 -0600

    scraping completed

--- scraping/index.php
+++ scraping/index.php
@@ -3,57 +3,86 @@
     
     include('simple_html_dom.php');
 
-    //link
-    $link = "https://www.travelandleisure.com/trip-ideas/nature-travel/most-scenic-campgrounds-in-us";
+    $link = "https://www.allrecipes.com/recipes/700/world-cuisine/asian/korean/";
 
-    $locations = file_get_html($link);
-
-    $destinations = [];
     $filenames = [];
-    $descriptions = [];
+    $menu_item_names = [];
+    $menu_item_descriptions = [];
+    $image_urls = [];
 
-    //save destination locations and filenames for the images
-    foreach ($locations->find('.mntl-sc-block-heading__text') as $location){
-        $name = trim(str_get_html($location)->plaintext);
-        $destinations[] = $name;
-        //
-        $filename = strtolower($name);
-        $filename = str_replace(',', '', $filename);
-        $filename = str_replace('.', '', $filename);
-        $filename = str_replace(' ', '-', $filename);
-        $filenames[] = $filename;
-    }
+    $menu_items = 9;
 
-    //save destination descriptions
-    $pos = -1;
-    foreach ($locations->find('.mntl-sc-block-html') as $description){
-       $pos++;
-       if($pos > 2){
-            $descriptions[] = trim($description->plaintext);
-       }
-    }
+    $items = file_get_html($link);
 
-    //get and download destination images
-    $index = 0;
-    /*foreach ($locations->find('.universal-image__image') as $image){
-        if($image->src != ""){
-            if($index < count($filenames)){
-                file_put_contents("../camping/imgs/location-".$filenames[$index].".jpg", file_get_contents($image->src));
-                $index++;
+    //save meals info and filenames for the images
+    foreach ($items->find('.card--no-image') as $item){
+        
+        $title = $item->find('.card__title-text');
+        
+        $details = file_get_html($item->href);
+        $description = $details->find('.article-subheading');
+        $image = $details->find('.primary-image__image');
+
+        if(count($image) > 0){
+            $image_urls[] = $image[0]->src;
+        }else{
+            $replace_img = $item->find('.universal-image__image');
+            if(count($replace_img) > 0){
+                $image_urls[] = $replace_img[0]->{'data-src'};
             }
         }
-    }*/
+       
+        if(count($description) > 0){
+            if($menu_items == 0) break;
 
-    for($i=0; $i<24; $i++){
+            $menu_item_names[] = trim($title[0]->plaintext);
+            $menu_item_descriptions[] = $description[0]->plaintext;
+            
+            $filename = strtolower(trim($title[0]->plaintext));
+            $filename = str_replace(' ', '-', $filename);
+            $filenames[] = $filename;
+
+            $menu_items--;
+        }
+
+    }
+
+   
+
+    foreach($filenames as $index=>$item){
+        echo "******************";
+        echo "<br>";
+        echo $item;
+        echo "<br>";
+        echo $menu_item_names[$index];
+        echo "<br>";
+        echo $menu_item_descriptions[$index];
+        echo "<br>";
+        echo $image_urls[$index];
+        echo "<br>";
+        
+        //$menu_items--;
+        //if($menu_items == 0) break;
+    }
+
+    //get and download images
+    foreach ($filenames as $index=>$image){
+        file_put_contents("./images/".$image.".jpg", file_get_contents($image_urls[$index]));
+    }
+
+    for($i=0; $i<9; $i++){
         // Reference: https://medoo.in/api/insert
         $database->insert("tb_dishes",[
-            "nombre"=> $destinations[$i],
-            "nombre"=> "name",
-            "descripcion"=> $descriptions[$i],
-            "imagen"=> "location-".$filenames[$i].".jpg",
-            "precio"=> 36.50
+            "nombre"=>$menu_item_names[$i],
+            "imagen"=> $filenames[$i] . ".jpg",
+            "categoria"=>null,
+            "destacado"=>null,
+            "descripcion"=>$menu_item_descriptions[$i],
+            "personas"=>rand(1,3),
+            "precio"=>rand(1 * 10, 70 * 10) / 10,
         ]);
     }
+    
     
 
 ?>
