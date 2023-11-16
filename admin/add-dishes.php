b49bcb81e1f957cb94e461bdd4975b37566f64e8
PuzzledStone <ianxd03@gmail.com>
2023-11-16 01:12:04 -0600

    add new dishes to the data base

---
+++ admin/add-dishes.php
@@ -0,0 +1,146 @@
+<?php 
+    /***
+     * 0. include database connection file
+     * 1. receive form values from post and insert them into the table (match table field with values from name atributte)
+     * 2. for the destination_image insert the value "destination-placeholder.webp"
+     * 3. redirect to destinations-list. php after complete the insert into
+     */
+
+     require_once '../database.php';
+
+    
+
+     // Reference: https://medoo.in/api/select
+     $categories = $database->select("tb_categories","*");
+
+     $message = "";
+
+     if($_POST){
+
+        if(isset($_FILES["imagen"])){
+            $errors = [];
+            $file_name = $_FILES["imagen"]["name"];
+            $file_size = $_FILES["imagen"]["size"];
+            $file_tmp = $_FILES["imagen"]["tmp_name"];
+            $file_type = $_FILES["imagen"]["type"];
+            $file_ext_arr = explode(".", $_FILES["imagen"]["name"]);
+
+            $file_ext = end($file_ext_arr);
+            $img_ext = ["jpeg", "png", "jpg", "webp"];
+
+            if(!in_array($file_ext, $img_ext)){
+                $errors[] = "File type is not valid";
+                $message = "File type is not valid";
+            }
+
+            if(empty($errors)){
+                $filename = strtolower($_POST["nombre"]);
+                $filename = str_replace(',', '', $filename);
+                $filename = str_replace('.', '', $filename);
+                $filename = str_replace(' ', '-', $filename);
+                $img = "dish-".$filename.".".$file_ext;
+                move_uploaded_file($file_tmp, "../scraping/images/".$img);
+
+                $category_name = "";
+                foreach ($categories as $category) {
+                    if ($category["id_categoria"] == $_POST["categoria"]) {
+                        $category_name = $category["nombre_categoria"];
+                        break;
+                    }
+                }
+
+                $database->insert("tb_dishes",[
+                    "id_categoria"=>$_POST["categoria"],
+                    "nombre_categoria" => $category_name,
+                    "nombre"=>$_POST["nombre"],
+                    "descripcion"=>$_POST["descripcion"],
+                    "imagen"=> $img,
+                    "precio"=>$_POST["precio"],
+                    "personas"=>$_POST["personas"],
+                    "destacado"=>$_POST["destacado"]
+                ]);
+            }
+        }
+        
+     }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Destination</title>
+    <link rel="stylesheet" href="../css/themes/admin.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Add New Dish</h2>
+        <?php 
+            echo $message;
+        ?>
+        <form method="post" action="add-dishes.php" enctype="multipart/form-data">
+            <div class="form-items">
+                <label for="nombre">Dish Name</label>
+                <input id="nombre" class="textfield" name="nombre" type="text">
+            </div>
+           
+            <div class="form-items">
+                <label for="categoria">Dish Category</label>
+                <select name="categoria" id="categoria">
+                    <?php 
+                        foreach($categories as $category){
+                            echo "<option value='".$category["id_categoria"]."'>".$category["nombre_categoria"]."</option>";
+                        }
+                    ?>
+                </select>
+                <input type="hidden" name="nombre_categoria" value="">
+            </div>
+
+            <div class="form-items">
+                <label for="descripcion">Dish Description</label>
+                <textarea id="descripcion" name="descripcion" id="" cols="30" rows="10"></textarea>
+            </div>
+
+            <div class="form-items">
+                <label for="imagen">Dish Image</label>
+                <img id="preview" src="../imgs/resources/destination-placeholder.webp" alt="Preview">
+                <input id="imagen" type="file" name="imagen" onchange="readURL(this)">
+            </div>
+
+            <div class="form-items">
+                <label for="precio">Dish Price</label>
+                <input id="precio" class="textfield" name="precio" type="text">
+            </div>
+
+            <div class="form-items">
+                <label for="personas">Cantidad de Personas</label>
+                <input id="personas" class="textfield" name="personas" type="text">
+            </div>
+
+            <div class="form-items">
+                <label for="destacado">Destacado</label>
+                <input id="destacado" class="textfield" name="destacado" type="text">
+            </div>
+
+            <div class="form-items">
+                <input class="submit-btn" type="submit" value="Add New Dish">
+            </div>
+        </form>
+    </div>
+
+    <script>
+        function readURL(input) {
+            if(input.files && input.files[0]){
+                let reader = new FileReader();
+
+                reader.onload = function(e) {
+                    let preview = document.getElementById('preview').setAttribute('src', e.target.result);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        
+    </script>
+    
+</body>
+</html>
